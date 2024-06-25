baee5e2372fba17a2f5445302ae189d6ab015c04
Rohan <rohan1990@gmail.com>
2024-06-25 20:02:35 +0100

    - Adding Pip - Fixing deprecated New Cases and Latest Update

--- Webscrape/Webscrape.py
+++ Webscrape/Webscrape.py
@@ -45,11 +45,11 @@ r1=soup.find_all("div", class_="news_date")
 r_fin = r_latest_cases[:r[6].text.find("new")].strip()
 
 
-print(f"Corona Cases: {x[4].text.replace(' ','')}")
-print(f"Deaths: {x[5].text}")
-print(f"Recovered: {x[6].text}")
-print(y1[n1:(n2+3)])
-print(f"New Cases: {r_fin}")
-print(f"Latest update on: {r1[0].text}")
+print(f"Corona Cases: {x[3].text.replace(' ','')}")
+print(f"Deaths: {x[4].text}")
+print(f"Recovered: {x[5].text}")
+#print(y1[n1:(n2+3)])
+#print(f"New Cases: {r_fin}")
+#print(f"Latest update on: {r1[0].text}")
 
 
